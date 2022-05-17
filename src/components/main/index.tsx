4a1a5941be88ab6faf674a3e0c1a822f994cd414
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-16 21:47:58 -0300

    create styles and structure

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,21 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <div>
-      <h1>tega</h1>
-    </div>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>React Avaçado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration"
+        alt="Um desenvolvedor de frente para o computador"
+      ></S.Illustration>
+    </S.Wrapper>
   )
 }
 
